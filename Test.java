a30ce32e4ad226a80cb0eecbd521d49afda528e7
rkeyur1234 <rkeyur1234@gmail.com>
2024-03-22 13:27:41 -0400

    Update Test.java
    added greeting-keyur

--- Test.java
+++ Test.java
@@ -4,8 +4,11 @@ public class Test {
     int addition(int a, int b){
         return (a+b);
     }
-}
-public void printGreeting()
-{
-	System.out.println("Hello from Nihar Patel")
+
+    public void printGreeting()
+    {
+        System.out.println("Hello from Nihar Patel");
+        System.out.println("Hello from Keyur Rana");
+        System.out.println("yo");
+    }
 }
